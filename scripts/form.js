cbfc69eb5dd1a16f52ac7c368033e9315cef1d8a
mikeokello <gokello@byupathway.edu>
2025-12-02 12:56:36 +0300

    initial changes

--- scripts/form.js
+++ scripts/form.js
@@ -1,15 +1,20 @@
-const products = [
-    { id: "gw-2025", name: "Golden Watches" },
-    { id: "wm-2025", name: "Washing Machines" },
-    { id: "tv-2025", name: "Smart TVs" },
-    { id: "s24-2025", name: "Samsung S24 Phones" }
-];
+document.addEventListener("DOMContentLoaded", () => {
 
-const productSelect = document.querySelector('#product');
+    const products = [
+        { id: "sp-2025", name: "Smart Phones" },
+        { id: "cl-2025", name: "Clothes" },
+        { id: "sw-2025", name: "Smart Watches" },
+        { id: "lp-2025", name: "Laptops" },
+        { id: "tv-2025", name: "Smart TVs" }
+    ];
 
-products.forEach(product => {
-    const option = document.createElement('option');
-    option.value = product.name;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
+    const productSelect = document.querySelector('#product');
+
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.name;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+
 });
